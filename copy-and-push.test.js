68af8e1a889cc56b23337f8b3ecb92a6a098202b
patrick hrabos <phrabos@gmail.com>
2021-03-22 18:47:00 -0400

    refactor function using spread and add test to make sure original array is unchanged

--- copy-and-push.test.js
+++ copy-and-push.test.js
@@ -1,10 +1,20 @@
 const copyAndPush = require('./copy-and-push');
 
-describe('some test', () => {
+describe('copy and push function', () => {
   it('returns a new array with all items in the original array and a new item pushed to the end', () => {
     const numbers = [1, 2, 3];
     const newArr = copyAndPush(numbers, 4);
 
     expect(newArr).toEqual([1, 2, 3, 4]);
+
   });
+
+  it('ensures the original array stays the same', () => {
+    const numbers = [1, 2, 3];
+    copyAndPush(numbers, 4);
+
+    expect(numbers).toEqual([1, 2, 3]);
+
+  });
+  
 });
