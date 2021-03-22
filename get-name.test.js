5fac40606bb220402aa6698cb33365addd03670a
patrick hrabos <phrabos@gmail.com>
2021-03-22 17:26:42 -0400

    add test for the character object using bracket notation

--- get-name.test.js
+++ get-name.test.js
@@ -7,4 +7,11 @@ describe('get name property from an object', () => {
 
     expect(objName).toEqual('spot');
   });
+
+  it('takes in an object and return the name property', () => {
+    const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+    const objName = getName(character);
+
+    expect(objName).toEqual('Aang');
+  });
 });
